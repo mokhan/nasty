a3e67600ae92d4fcae759fc0c531c95a223b0f81
mo khan <mo@mokhan.ca>
2013-12-27 10:22:19 -0700

    use eval instead of erb in spec.

--- spec/unit/expose_binding_spec.rb
+++ spec/unit/expose_binding_spec.rb
@@ -3,14 +3,12 @@ require "spec_helper"
 module Nasty
   describe ExposeBinding do
     it "exposes an objects private binding" do
-      item = Object.new
-      item.instance_eval do
+      item = Object.new.instance_eval do
         @message = "secret"
+        self
       end
       item.extend(Nasty::ExposeBinding)
-      binding = item.get_binder
-      result = ERB.new("<%= @message %>").result(binding)
-      result.should == "secret"
+      eval("@message", item.get_binder).should == "secret"
     end
   end
 end
